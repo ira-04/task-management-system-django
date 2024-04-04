2b421edb388198c8e8f3a8059cee23e69a83eb84
ira_04 <irasurati78@gmail.com>
2024-04-04 23:32:23 +0530

    Task_management_system

---
+++ base/models.py
@@ -0,0 +1,35 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone  
+
+# Create your models here.
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    # deadline = models.DateTimeField(null=True, blank=True)  
+    # PRIORITY_CHOICES = [
+    #     ('LOW', 'Low'),
+    #     ('MEDIUM', 'Medium'),
+    #     ('HIGH', 'High'),
+    # ]
+    # priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='MEDIUM')  
+    # deadline = models.DateTimeField()
+    deadline = models.DateTimeField(default=timezone.now() + timezone.timedelta(days=1))  # Set default to one day from now
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='low')
+    done = models.BooleanField(default=False)
+    complete = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        order_with_respect_to = 'user'
